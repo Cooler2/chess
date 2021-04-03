b0c560d61d4d12399774d268f626adc9429cdc14
Cooler <ivan@apus-software.com>
2021-04-03 13:19:44 +0300

    Improved tree visualization

--- TreeView.pas
+++ TreeView.pas
@@ -96,7 +96,7 @@ procedure UpdateTreePos(clientHeight:integer);
 
 procedure DrawTreeLines(canvas:TCanvas);
  var
-  i,j,d,y:integer;
+  i,j,d,y,yn:integer;
  begin
   for d:=1 to high(treeData) do begin
    if length(treeData[d].items)=0 then exit;
@@ -104,16 +104,27 @@ procedure DrawTreeLines(canvas:TCanvas);
    for i:=0 to high(treeData[d-1].items) do
     if treeData[d-1].items[i]=selidx[d-1] then
      with treeData[d] do begin
+      yn:=0;
       for j:=0 to high(items) do begin
+       if items[j]=selIdx[d] then begin
+        canvas.Pen.Width:=2;
+        yn:=pos[j];
+       end else
+        canvas.Pen.Width:=1;
        canvas.moveto(25+d*COL_WIDTH-COL_WIDTH div 2,pos[j]);
        canvas.lineto(25+d*COL_WIDTH-COL_WIDTH div 2+10,pos[j]);
       end;
       // вертикальная линия
       canvas.moveto(25+d*COL_WIDTH-COL_WIDTH div 2,pos[0]);
       canvas.lineto(25+d*COL_WIDTH-COL_WIDTH div 2,pos[j-1]);
-      // горизонтальная линия
+      canvas.Pen.Width:=2;
       y:=treeData[d-1].pos[i];
-      canvas.moveto(25+d*COL_WIDTH-COL_WIDTH div 2-10,y);
+      if yn>0 then begin
+       canvas.moveto(25+d*COL_WIDTH-COL_WIDTH div 2,y);
+       canvas.lineto(25+d*COL_WIDTH-COL_WIDTH div 2,yn);
+      end;
+      // горизонтальная линия
+      canvas.moveto(25+d*COL_WIDTH-COL_WIDTH div 2-15,y);
       canvas.lineto(25+d*COL_WIDTH-COL_WIDTH div 2,y);
       break;
      end;
@@ -122,7 +133,7 @@ procedure DrawTreeLines(canvas:TCanvas);
 
 procedure DrawTreeNodes(canvas:TCanvas);
  var
-  i,j,d,idx,dbInd,cx,cy:integer;
+  i,j,d,idx,dbInd,cx,cy,x:integer;
   st,st2,st3:string;
   val:single;
   cell1,cell2:byte;
@@ -136,9 +147,9 @@ procedure DrawTreeNodes(canvas:TCanvas);
     if d>0 then begin
      idx:=items[0];
      j:=0; // здесь будет решающий
-     if data[idx].depth mod 2>0 then val:=-10000 else val:=10000;
+     if data[idx].whiteTurn xor playerWhite then val:=-10000 else val:=10000;
      for i:=0 to high(items) do begin
-      if data[idx].depth mod 2>0 then begin
+      if data[idx].whiteTurn xor playerWhite then begin
        if data[items[i]].rate>val then begin val:=data[items[i]].rate; j:=i; end;
       end else begin
        if data[items[i]].rate<val then begin val:=data[items[i]].rate; j:=i; end;
@@ -147,32 +158,53 @@ procedure DrawTreeNodes(canvas:TCanvas);
     end;
 
     for i:=0 to high(items) do begin
-     if selidx[d]=items[i] then canvas.brush.color:=$C0D0E0
-      else begin
-       if data[items[i]].flags and movVerified=0 then canvas.brush.color:=$E0E0E0
-        else canvas.brush.color:=$E0C0C0;
-      end;
-     if i=j then canvas.pen.color:=$A0 else canvas.pen.color:=$101010;
+     if selidx[d]=items[i] then // выбранный элемент
+      canvas.Pen.Width:=2
+     else begin
+      canvas.Pen.Width:=1;
+      if data[items[i]].flags and movVerified=0 then canvas.brush.color:=$E0E0E0
+       else canvas.brush.color:=$E0C0C0;
+     end;
+     if i=j then canvas.brush.color:=$D4F0D4 // элемент с наилучшей оценкой
+      else
+       // фон обычного элемента
+       if not data[items[i]].whiteTurn then
+        canvas.brush.color:=$F8F8F8
+       else
+        canvas.brush.color:=$D8D8D8;
      canvas.font.color:=canvas.pen.color;
      // нет ли элемента в базе?
-     hash:=BoardHash(data[items[i]]);
-     for dbInd:=0 to high(dbItems) do
+     (*hash:=BoardHash(data[items[i]]);
+     for dbInd:=0 to high(dbItems) do /// TODO: оптимизировать
       if dbItems[dbInd].hash=hash then
-       canvas.font.color:=$8000;
+       canvas.font.color:=$8000; *)
 
      cx:=25+d*COL_WIDTH;
      cy:=pos[i];
-     if d>0 then
-      canvas.RoundRect(cx-COL_WIDTH div 2+6,cy-7,cx+COL_WIDTH div 2-6,cy+7,5,5)
-     else
+     if d>0 then begin
+      canvas.RoundRect(cx-COL_WIDTH div 2+6,cy-7,cx+COL_WIDTH div 2-6,cy+8,5,5);
+      if data[idx].firstChild>0 then begin
+       x:=cx+COL_WIDTH div 2-9;
+       canvas.MoveTo(x,cy-6);
+       canvas.LineTo(x,cy+7);
+      end;
+     end else
       canvas.RoundRect(cx-22,cy-12,cx+32,cy+12,7,7);
      idx:=items[i];
      if d>0 then begin
       cell1:=data[idx].lastTurnFrom;
       cell2:=data[idx].lastTurnTo;
-      if data[idx].flags and movBeat>0 then ch:=':' else ch:='-';
-      if data[idx].flags and movCheck>0 then st2:='+' else st2:='';
-       st3:=FloatToStrF(data[idx].rate,ffFixed,4,3);
+      if data[idx].flags and movBeat>0 then begin // ход со взятием
+       ch:=':';
+       canvas.Font.Color:=$B0;
+      end else
+       ch:='-';
+      if data[idx].flags and movCheck>0 then begin // шах
+       st2:='+';
+       canvas.Font.Color:=$A000B0;
+      end else
+       st2:='';
+     st3:=FloatToStrF(data[idx].rate,ffFixed,4,3);
      if abs(data[idx].rate)>210 then
        st3:=FloatToStrF(data[idx].rate,ffFixed,4,0);
       st:=NameCell(cell1 and $F,cell1 shr 4)+ch+NameCell(cell2 and $F,cell2 shr 4)+st2+
@@ -227,7 +259,7 @@ begin
  i:=1;
  with treeData[col] do
   for i:=0 to high(items) do
-   if (y>=pos[i]-7) and (y<=pos[i]+7) then begin
+   if (y>=pos[i]-8) and (y<=pos[i]+8) then begin
     idx:=items[i];
     selidx[col]:=idx;
     selIdx[col+1]:=-1;
@@ -236,11 +268,11 @@ begin
      v:=items[i];
      ShowMessage(inttostr(v)+' '+data[v].weight.ToString,'');
     end;
+    SetLength(treeData,col+2);
+    BuildTree;
     mainForm.DrawBoard(sender);
     break;
    end;
- SetLength(treeData,col+2);
- BuildTree;
  Invalidate;
  MainForm.Estimate(true);
 end;
